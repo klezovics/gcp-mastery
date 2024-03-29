2c686c233a444179e523d777543a4709a0189e3d
klezovich <arthur@embeddable.de>
2024-03-29 13:38:16 +0100

    first commit

---
+++ src/main/kotlin/com/klezovich/gcp_mastery/GcpMasteryApplication.kt
@@ -0,0 +1,11 @@
+package com.klezovich.gcp_mastery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GcpMasteryApplication
+
+fun main(args: Array<String>) {
+    runApplication<GcpMasteryApplication>(*args)
+}
---
+++ src/test/kotlin/com/klezovich/gcp_mastery/GcpMasteryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.klezovich.gcp_mastery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GcpMasteryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
